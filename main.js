1ae4cda285df1e9b303c7766584097f11d3d476e
JoseArjona <j.angel.matheis@gmail.com>
2023-05-15 16:23:09 -0600

    Mobile Menu

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
 const linkEmail = document.querySelector('.navbar-email');
+const burguerIcon = document.querySelector('#burguerIcon');
 const accountMenu = document.querySelector('#accountMenu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+function toggleMenus(selectMenu) {
+    selectMenu.classList.toggle('inactive');
+}
 
 linkEmail.addEventListener('click', () => {
     console.log('click Email');
-    accountMenu.classList.toggle('inactive');
+    toggleMenus(accountMenu);
+});
+
+burguerIcon.addEventListener('click', () => {
+    console.log('click Burguer');
+    toggleMenus(mobileMenu);
 });
