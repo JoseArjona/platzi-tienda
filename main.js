aecb66a3db740e4f6538b95ca75e3d02ef460a37
JoseArjona <j.angel.matheis@gmail.com>
2023-05-15 18:34:03 -0600

    Menu shopping cart

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const linkEmail = document.querySelector('.navbar-email');
+const mobileMenu = document.querySelector('.mobile-menu');
+const productDetails = document.querySelector('.product-detail');
 const burguerIcon = document.querySelector('#burguerIcon');
 const accountMenu = document.querySelector('#accountMenu');
-const mobileMenu = document.querySelector('.mobile-menu');
+const shoppingCart = document.querySelector('#shoppingCart');
 
 function toggleMenus(selectMenu) {
     selectMenu.classList.toggle('inactive');
@@ -15,4 +17,11 @@ linkEmail.addEventListener('click', () => {
 burguerIcon.addEventListener('click', () => {
     console.log('click Burguer');
     toggleMenus(mobileMenu);
+    productDetails.classList.add('inactive');
+});
+
+shoppingCart.addEventListener('click', () => {
+    console.log('click Cart');
+    toggleMenus(productDetails);
+    mobileMenu.classList.add('inactive');
 });
